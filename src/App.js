aec0ca6582c69fa21a2a9cd3b704cd4f421e4c1a
Your Name <kamraj089@gmail.com>
2022-08-22 22:41:36 +0530

    ddda

--- src/App.js
+++ src/App.js
@@ -1,23 +1,74 @@
 import logo from './logo.svg';
 import './App.css';
-
+import {useEffect, useState} from 'react';
+import * as tf from "@tensorflow/tfjs";
+import { loadLayersModel } from "@tensorflow/tfjs";
 function App() {
+   const [image,setImage] = useState(null)
+   const [modelUrl,setModelUrl] = useState("")
+
+
+
+  const processImage = (imageData)=>{
+    let imageTensor = tf.browser.fromPixels(imageData);
+    const offset = tf.scalar(255.0)
+    const normalized = tf.scalar(1.0).sub(imageTensor.div(offset))
+    const batched = normalized.expandDims(0);
+    return batched
+  }
+  const predict = async()=>{
+    if(!image){
+      alert("Please Add Image")
+    }
+    const MODEL_HTTP_URL = "model.json";
+    try {
+      const model = await loadLayersModel(
+        window.location.href + MODEL_HTTP_URL
+      );
+      console.log("Model loaded from HTTP.");
+      console.log(model, "mmm");
+        const imaged = document.getElementById("image");
+        console.log("converting image ...");
+        const imageTensor = processImage(imaged);
+        console.log("loading model ...");
+           let result = model.predict(imageTensor);
+           console.log(result, "result");
+    } catch (error) {
+      console.error(error, "rrrr");
+    }
+   
+ 
+
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input
+        type={"file"}
+        alt={"hh"}
+        onChange={(e) => {
+          const [file] = e.target.files;
+          setImage(URL.createObjectURL(file));
+        }}
+      />
+      {/* <input
+        type={"file"}
+        alt={"ggjgjg"}
+        onChange={(e) => {
+          const [file] = e.target.files;
+          console.log(URL.createObjectURL(file));
+        }}
+      /> */}
+      <img
+        id="image"
+        alt="kk"
+        height={"200"}
+        src={
+          image
+        }
+        width={"300"}
+      />
+
+      <button onClick={predict}>Predict</button>
     </div>
   );
 }
