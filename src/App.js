88bd92535759f14876a306a74b7bb8d131051d68
Your Name <kamraj089@gmail.com>
2022-10-10 22:52:05 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,74 +1,47 @@
 import logo from './logo.svg';
 import './App.css';
 import {useEffect, useState} from 'react';
-import * as tf from "@tensorflow/tfjs";
-import { loadLayersModel } from "@tensorflow/tfjs";
+
 function App() {
-   const [image,setImage] = useState(null)
-   const [modelUrl,setModelUrl] = useState("")
-
-
-
-  const processImage = (imageData)=>{
-    let imageTensor = tf.browser.fromPixels(imageData);
-    const offset = tf.scalar(255.0)
-    const normalized = tf.scalar(1.0).sub(imageTensor.div(offset))
-    const batched = normalized.expandDims(0);
-    return batched
-  }
-  const predict = async()=>{
-    if(!image){
-      alert("Please Add Image")
-    }
-    const MODEL_HTTP_URL = "model.json";
-    try {
-      const model = await loadLayersModel(
-        window.location.href + MODEL_HTTP_URL
-      );
-      console.log("Model loaded from HTTP.");
-      console.log(model, "mmm");
-        const imaged = document.getElementById("image");
-        console.log("converting image ...");
-        const imageTensor = processImage(imaged);
-        console.log("loading model ...");
-           let result = model.predict(imageTensor);
-           console.log(result, "result");
-    } catch (error) {
-      console.error(error, "rrrr");
-    }
    
- 
-
+const [url,setUrl] = useState("")
+const [message,setMessage] = useState('')
+const [messages,setMessages] = useState([])
+const [socketInstance,setSocketInstance]= useState(null)
+  const send = ()=>{
+      socketInstance.send(message);
   }
+  useEffect(()=>{
+     const socket = new WebSocket("ws://localhost:8080");
+     console.log(socket,"sss")
+     socket.addEventListener("open", (event) => {
+       socket.send("Hello Server!");
+     });
+     socket.addEventListener("message", (event) => {
+      setMessages([...messages,event.data])
+       console.log("Message from server ", event.data);
+     });
+     setSocketInstance(socket)
+
+     return ()=>{
+        socket.close()
+     }
+  },[])
   return (
     <div className="App">
-      <input
-        type={"file"}
-        alt={"hh"}
-        onChange={(e) => {
-          const [file] = e.target.files;
-          setImage(URL.createObjectURL(file));
-        }}
-      />
-      {/* <input
-        type={"file"}
-        alt={"ggjgjg"}
-        onChange={(e) => {
-          const [file] = e.target.files;
-          console.log(URL.createObjectURL(file));
-        }}
-      /> */}
-      <img
-        id="image"
-        alt="kk"
-        height={"200"}
-        src={
-          image
-        }
-        width={"300"}
-      />
-
-      <button onClick={predict}>Predict</button>
+      <h1>Socket test</h1>
+      <div>
+        <input
+          value={message}
+          placeholder="Enter your message"
+          style={{ width: 500 }}
+          onChange={(e) => setMessage(e.target.value)}
+        />
+        <button style={{ marginLeft: 10 }} onClick={send}>
+          send
+        </button>
+      </div>
+ 
     </div>
   );
 }
